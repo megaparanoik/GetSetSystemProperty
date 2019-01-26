56528c050fa4ae952c345cbaf348214d3aee4093
Androsov Aleksandr <eelleekk@gmail.com>
2019-01-26 18:58:01 +0200

    Add Reflection

--- app/src/main/java/com/example/setprop/MainActivity.java
+++ app/src/main/java/com/example/setprop/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.setprop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Date;
+import java.util.Timer;
+
+import static android.webkit.ConsoleMessage.MessageLevel.LOG;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -18,6 +23,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     TextView EXT;
     TextView Status;
     String result = " ";
+    Date timer;
+    boolean lock_update;
+    String TAG = "GetSetProp";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +39,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         EXT = findViewById(R.id.idExt);
         Status = findViewById(R.id.idStatus);
 
+        timer = new Date();
+        lock_update = false;
 
-        FW.setText(getString(R.string.fw_ver) + "   Unknown");
-        HW.setText(getString(R.string.hw_ver) + "   Unknown");
-        EXT.setText(getString(R.string.extensions) + "   Unknown");
-        Status.setText(getString(R.string.update_status) + " Halt");
+        FW.setText(getString(R.string.fw_ver) + "Unknown");
+        HW.setText(getString(R.string.hw_ver) + "Unknown");
+        EXT.setText(getString(R.string.extensions) + "Unknown");
+        Status.setText(getString(R.string.update_status) + "Halt");
 
         btnUpdateInfo.setOnClickListener(this);
         btnUpdateFw.setOnClickListener(this);
@@ -44,55 +54,81 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
 
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.id_update_button:
-                FW.setText(getString(R.string.fw_ver) + "   " + getSystemProperty("hw.gps.fw"));
-                HW.setText(getString(R.string.hw_ver) + "   " + getSystemProperty("hw.gps.hw"));
+                FW.setText(getString(R.string.fw_ver) + SystemPropertiesProxy.get("hw.gps.fw", "Unknown"));
+                HW.setText(getString(R.string.hw_ver) + SystemPropertiesProxy.get("hw.gps.hw", "Unknown"));
 
                 int i = 1;
                 String extstring = "";
-                while(!getSystemProperty("hw.gps.ext" + i).isEmpty()) {
-                    extstring += getSystemProperty("hw.gps.ext" + i) + ",";
+
+                while ( !SystemPropertiesProxy.get(String.format("hw.gps.ext%d", i)).equals("") ) {
+                    extstring += SystemPropertiesProxy.get(String.format("hw.gps.ext%d", i)) + ",";
                     i++;
-                    if(i > 20)
+                    if (i > 20) {
                         break;
+                    }
                 }
-                EXT.setText(getString(R.string.extensions) + "   " + extstring);
+                EXT.setText(getString(R.string.extensions) + extstring);
 
                 break;
 
             case R.id.id_update_fw_button:
-                    setSystemProperty("hw.gps.update", "start");
+                if (!lock_update){
+                    lock_update = true;
 
-                new Thread(new Runnable() {
-                    public void run() {
-                        result = " ";
-                        while( !result.equals("Complete") && !result.equals("Fail") ) {
-                            result = getSystemProperty("hw.gps.update");
-                            //Status.setText(result);
-                            Status.post(new Runnable() {
+                    SystemPropertiesProxy.set("hw.gps.update", "start");
+
+                    new Thread(new Runnable() {
+                        public void run() {
+                            result = " ";
+
+                            timer.setTime(0);
+
+                            while (!result.equals("Complete") && !result.equals("Fail")) {
+                                result = SystemPropertiesProxy.get("hw.gps.update", "Unknown");
+                                //Status.setText(result);
+                                Status.post(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Status.setText(getText(R.string.update_status) + result);
+                                    }
+                                });
+
+                                try {
+                                    Thread.sleep(1000);
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+
+                                timer.setSeconds(timer.getSeconds() + 1);
+
+                                btnUpdateFw.post(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        btnUpdateFw.setText(String.format("00:%02d:%02d", timer.getMinutes(), timer.getSeconds()));
+                                    }
+                                });
+                            }
+
+                            lock_update = false;
+
+                            btnUpdateFw.post(new Runnable() {
                                 @Override
                                 public void run() {
-                                    Status.setText(getText(R.string.update_status) + " " + result);
+                                    btnUpdateFw.setText(getString(R.string.update_fw_btn));
                                 }
                             });
 
-                            try {
-                                Thread.sleep(500);
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
+                            return;
                         }
-                        return;
-                    }
-                }).start();
-
-
+                    }).start();
+                }
                 break;
         }
 
     }
-
+/*
     private String getSystemProperty(String propertyName) {
         String propertyValue = "[UNKNOWN]";
 
@@ -126,5 +162,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         return retValue;
     }
-
+*/
 }
